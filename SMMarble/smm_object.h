99ff269c8bbcfc18e2afd91a465052457ac59779
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-12-21 18:41:22 +0900

    Update smm_object.h

--- SMMarble/smm_object.h
+++ SMMarble/smm_object.h
@@ -2,7 +2,7 @@
 //  smm_object.h
 //  SMMarble object
 //
-//  Created by Juyeop Kim on 2023/11/05.
+//  Created by Seoyeon Kim.
 //
 
 #ifndef smm_object_h
@@ -17,39 +17,45 @@
 #define SMMNODE_TYPE_FESTIVAL           6
 
 #define SMMNODE_TYPE_MAX                7
+#define MAX_CHARNAME                    200
 
+typedef enum smmObjType {
+    smmObjType_board = 0,
+    smmObjType_card,
+    smmObjType_grade
+} smmObjType_e;
 
-/* node type :
-    lecture,
-    restaurant,
-    laboratory,
-    home,
-    experiment,
-    foodChance,
-    festival
-*/
+static char smmNodeName[SMMNODE_TYPE_MAX][MAX_CHARNAME] = {
+       "강의",
+       "식당",
+       "실험실",
+       "집",
+       "실험실로이동",
+       "음식찬스",
+       "축제시간"
+};
 
-
-/* grade :
-    A+,
-    A0,
-    A-,
-    B+,
-    B0,
-    B-,
-    C+,
-    C0,
-    C-
-*/
-
-
+typedef enum smmObjGrade {
+    smmObjGrade_Ap = 0,
+    smmObjGrade_A0,
+    smmObjGrade_Am,
+    smmObjGrade_Bp,
+    smmObjGrade_B0,
+    smmObjGrade_Bm,
+    smmObjGrade_Cp,
+    smmObjGrade_C0,
+    smmObjGrade_Cm
+} smmObjGrade_e;
 
 //object generation
-void smmObj_genNode(char* name, int type, int credit, int energy);
+void* smmObj_genObject(char* name, smmObjType_e objType, int type, int credit, int energy, smmObjGrade_e grade);
 
 //member retrieving
-char* smmObj_getNodeName(int node_nr);
-int smmObj_getNodeType(int node_nr);
+char* smmObj_getNodeName(void* obj);
+int smmObj_getNodeType(void* obj);
+int smmObj_getNodeCredit(void* obj);
+int smmObj_getNodeEnergy(void* obj);
+int smmObj_getNodeGrade(void* obj);
 
 //element to string
 char* smmObj_getTypeName(int type);
